3d84f638ee9120a5b66611190f1350866fca5d0c
yevgeniylapin <yevgeniy.d.lapin@gmail.com>
2017-08-30 02:05:57 +0300

    data for 166 subjects

--- transform.py
+++ transform.py
@@ -4,12 +4,14 @@ from math import log
 from pymystem3 import Mystem
 import pymorphy2
 import operator
+import re
 
 with open('younger_august.csv', 'r', encoding = 'utf-8') as f_y: #вставь название файла про молодых
        younger_database = f_y.read().split('\n')
+
 with open('older_august.csv', 'r', encoding = 'utf-8') as f_o: #вставь название файла про пожилых
        older_database = f_o.read().split('\n')
-
+       
 with open('freq_young.csv', 'w', encoding = 'utf-8') as y:
      with open('freq_old.csv', 'w', encoding = 'utf-8') as o:
           with open('freq_all.csv', 'w', encoding = 'utf-8') as all:
@@ -18,6 +20,7 @@ with open('freq_young.csv', 'w', encoding = 'utf-8') as y:
                for line in younger_database[1:]:
                     line = line.split('\t')
                     s = line[4::4]
+                    number_of_younger = len(s)
                     dictionary = {}
                     for el in s:
                          if el in dictionary:
@@ -48,6 +51,7 @@ with open('freq_young.csv', 'w', encoding = 'utf-8') as y:
                for line in older_database[1:]:
                     line = line.split('\t')
                     s = line[4::4]
+                    number_of_older = len(s)
                     dictionary = {}
                     for el in s:
                          if el in dictionary:
@@ -71,6 +75,8 @@ with open('freq_young.csv', 'w', encoding = 'utf-8') as y:
                          to_write = to_write.replace('  ',' ')
                     o.write(to_write)
                     i += 1
+
+               total_number = number_of_older + number_of_younger
                
                arr_all = []
                arr_all_sorted = []
@@ -87,9 +93,20 @@ with open('freq_young.csv', 'w', encoding = 'utf-8') as y:
                     sorted_dictionary = sorted(dictionary.items(), key=operator.itemgetter(1), reverse = True)
                     arr_all_sorted.append(sorted_dictionary)
 
+                          
+
                i = 1
+               all_answers = []
                for dictionary in arr_all_sorted:
+                    to_write = str(i)+'\t'
+                    all_answers_to_write = ''
                     for element in dictionary:
+                         to_write = to_write + element[0] + ' ' + str(element[1]) + '\t'
+                    for element in dictionary:
+                         all_answers_to_write = all_answers_to_write + element[0] + ' '+ str(element[1]) + ', '
+                    all_answers.append(all_answers_to_write.strip(', '))
+                    to_write = to_write.strip('\t')+'\n'
+                    '''for element in dictionary:
                          to_write = str(i)+'\t'+str(dictionary[0:])+'\n'
                          to_write = to_write.replace(',',' ')
                          to_write = to_write.replace('[','')
@@ -99,8 +116,8 @@ with open('freq_young.csv', 'w', encoding = 'utf-8') as y:
                          to_write = to_write.replace(')','')
                          to_write = to_write.replace('(','')
                          to_write = to_write.replace('  ',' ')                         
-                         to_write = to_write.replace('  ',' ')                         
-                    all.write(to_write)
+                         to_write = to_write.replace('  ',' ')'''                        
+                    all.write(to_write.strip(','))
                     i += 1
 
 
@@ -111,18 +128,17 @@ with open('stimuli.txt', 'r', encoding = 'utf-8') as f:
 	stimuli = f.read().split('\n')
 	
 table_out = '№	Context	Dominant response	Number of completions	Ommitted completions or no completion	Number of unique responses	Alternative completions	Percentage of ending agreement %EA	H-statistic	Frequency (ipm)	Number of syllables\n'
-vow = 'ёуеоыаэяию'
+vow = ['ё','у','е','о','ы','а','э','я','и','ю']
 
 with open('freqrnc2011.csv', 'r', encoding='utf-8') as f:
         ipms = {i.split('\t')[0]: float(i.split('\t')[2]) for i in f.read().split('\n')[1:-1]}
 
 m = Mystem()
 for z, line in enumerate(table[:-1]):
-	number, values = line.split('\t')[0], line.strip(' ').split('\t')[0:]
-	print (number)
-	print (values)
-	values = dict(v.split() for v in values)
-	print(values)
+	number = line.split('\t')[0]
+	values = line[len(number)+1:]
+	regex = re.findall('\t?(.*?) (\d[0-9]*)', values)
+	values = dict(v for v in regex)
 
 	S = 0
 	
@@ -143,21 +159,24 @@ for z, line in enumerate(table[:-1]):
 	for k in values:
 		H += int(values[k])/S * log(S/int(values[k]), 2)
 
-	if lemmas in ipms:
-		ipm = str(ipms[lemmas])
-	elif lemmas == 'марк':
+	
+	if lemmas == 'марк':
 		ipm = str(ipms["марка"])
 	elif lemmas == 'почтить':
 		ipm = str(ipms["почта"])
+	elif lemmas == 'печь':
+		ipm = str(32.6)
+	elif lemmas in ipms:
+		ipm = str(ipms[lemmas])
 	else:
 		ipm = 'NA'
 	j = 0
-	for i in max_k:
+	for i in max_kk:
 		if i in vow:
 			j += 1
 
 
-	out_line = '%s\t%s\t%s\t%s\t%d\t%d\t%s\t%f\t%f\t%s\t%d\n' % (number, stimuli[z], max_kk, S, int(86-S), len(values), values_line, int(values[max_kk])*100/S, H, ipm, j)
+	out_line = '%s\t%s\t%s\t%s\t%d\t%d\t%s\t%f\t%f\t%s\t%d\n' % (number, stimuli[z], max_kk, S, int(total_number-S), len(values), all_answers[z], int(values[max_kk])*100/S, H, ipm, j)
 	table_out += out_line
 	max_k = 0
 	max_k_k = 0
